5c9323c2aaec13b95ef3bb01bf813938313ef1e6
javendorg <javendorg777@yandex.ru>
2025-11-03 16:48:14 +0300

    Architecture and data formation

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
+# labyrinth_game/main.py
+from .constants import ROOMS
+
 def main():
-    print("Первая попытка запустить проект!")
+    game_state = {
+        'player_inventory': [], # Инвентарь игрока
+        'current_room': 'entrance', # Текущая комната
+        'game_over': False, # Значения окончания игры
+        'steps_taken': 0 # Количество шагов
+    }
