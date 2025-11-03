8e6f203536a74a51905046be4dd0f80ba37b0859
javendorg <javendorg777@yandex.ru>
2025-11-03 22:44:02 +0300

    Rename project to project1_kizelbashev_alexander_m25-555

--- labyrinth_game/constants.py
+++ labyrinth_game/constants.py
@@ -11,21 +11,21 @@ ROOMS = {
         'exits': {'south': 'entrance', 'west': 'library', 'north': 'treasure_room'},
         'items': [],
         'puzzle': ('На пьедестале надпись: "Назовите число, которое идет после девяти". Введите ответ цифрой или словом.', '10'),
-        'reward': 'Rew_Hall'
+        'reward': 'rew_hall'
     },
     'trap_room': {
         'description': 'Комната с хитрой плиточной поломкой. На стене видна надпись: "Осторожно — ловушка".',
         'exits': {'west': 'entrance', 'south': 'dungeon'},
         'items': ['rusty_key'],
         'puzzle': ('Система плит активна. Чтобы пройти, назовите слово "шаг" три раза подряд (введите "шаг шаг шаг")', 'шаг шаг шаг'),
-        'reward': 'Rew_Trap_Room'
+        'reward': 'rew_trap_room'
     },
     'library': {
         'description': 'Пыльная библиотека. На полках старые свитки. Где-то здесь может быть ключ от сокровищницы.',
         'exits': {'east': 'hall', 'north': 'armory'},
         'items': ['ancient book'],
         'puzzle': ('В одном свитке загадка: "Что растет, когда его съедают?" (ответ одно слово)', 'резонанс'),  # намеренно странная загадка: можно сделать альтернативу
-        'reward': 'Rew_Library'
+        'reward': 'rew_library'
     },
     'armory': {
         'description': 'Старая оружейная комната. На стене висит меч, рядом — небольшая бронзовая шкатулка.',
@@ -38,7 +38,7 @@ ROOMS = {
         'exits': {'south': 'hall', 'east': 'shrine'},
         'items': ['treasure_chest'],
         'puzzle': ('Дверь защищена кодом. Введите код (подсказка: это число пятикратного шага, 2*5= ? )', '10'),
-        'reward': 'Rew_Treasure_Room'
+        'reward': 'rew_treasure_room'
     },
     # МОИ КОМНАТЫ
     'gallery': { #1
@@ -58,14 +58,14 @@ ROOMS = {
         'exits': {'west': 'treasure_room'},
         'items': [],
         'puzzle': ('На алтаре лежит послание: "Я всегда впереди, но никогда не двигаюсь. Кто я?" (ответ одно слово)', 'будущее'),
-        'reward': 'Rew_Shrine'
+        'reward': 'rew_shrine'
     },
     'barracks': { #4
         'description': 'Изношенное казарменное помещение с рядами пустых койок. Стены украшены боевыми знаменами и ржавыми оружиями.',
         'exits': {'south': 'armory'},
         'items': ['dagger'],
         'puzzle': ('Найдена надпись: "Я не живу, но расту; у меня нет легких, но мне нужен воздух. Что я?" (ответ одно слово)', 'огонь'),
-        'reward': 'Rew_Barracks'
+        'reward': 'treasure_key'
     }
 }
 
